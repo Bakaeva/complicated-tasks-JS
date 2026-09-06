1b79cc60796a9a4dc093d3401940458aa5c7f6e5
Bakaeva <l_bakaeva74@mail.ru>
2026-09-07 01:43:05 +0300

    Выполнено усложненное д/з урока 7 (объекты, массивы)

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
 'use strict';
 
-// 1) Создать массив arr = []
-// — Записать в него 7 любых многозначных чисел в виде строк
-// — Вывести в консоль только те, что начинаются с цифры 2 или 4 (Должны присутствовать в массиве)
+// Создать массив week и записать в него дни недели в виде строк
+// Вывести на экран все дни недели
+// Каждый из них с новой строчки
+// Выходные дни - курсивом
+// Текущий день - жирным шрифтом(использовать объект даты)
 
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
-}
+const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 
-const inputArray = function (elemCount) {
-  let arr = [];
-  let elem;
-  for (let i = 0; i < elemCount; i++) {
-    do {
-      elem = prompt(`Введите ${i + 1}-й элемент массива (он должtн быть многозначным положительным числом). (ESC/Отмена завершит выполнение программы)`);
-      if (elem === null)
-        return [];
-    } while (!isNumber(elem) || elem <= 0 || elem.trim().length < 2);
-    arr.push(elem.trim()); // элементы - строки без концевых пробелов, которые м.б. преобразованы в многозначное число
-  }
-  return arr;
-}
+let todayIndex = new Date().getDay();
+todayIndex = todayIndex === 0 ? 6 : todayIndex - 1;
 
-let arrayOfString = inputArray(7);
+for (const dayIndex in week) {
+  let styles = '';
+  if (dayIndex >= 5) {
+    styles += 'font-style: italic; ';
+  };
+  if (dayIndex == todayIndex) {
+    styles += 'font-weight: bold; ';
+  };
 
-if (arrayOfString.length > 0) {
-  console.log("Исходный массив: " + arrayOfString);
-  console.log("Числа, начинающиеся с 2 и 4: ");
-
-  // 1-й способ:  
-  for (let i = 0; i < arrayOfString.length; i++) {
-    if (arrayOfString[i].startsWith('2') || arrayOfString[i].startsWith('4')) {
-      console.log(arrayOfString[i]);
-    }
-  }
-
-  console.log("\n");
-  // 2-й способ:
-  for (const el of arrayOfString) {
-    if (el.startsWith('2') || el.startsWith('4'))
-      console.log(el);
-  }
-
-  console.log("\n");
-  // 3-й способ:
-  console.log(arrayOfString.filter(el => el.startsWith('2') || el.startsWith('4')));
-}
-
-// ---------------------------------------------------
-// 2) Вывести в столбик все простые числа от 1 до 100 (сделать при помощи цикла).
-//    Рядом с каждым числом написать оба делителя данного числа. Например: “Делители этого числа: 1 и n”
-
-const isPrime = function (num) {
-  for (let i = 2, limit = Math.sqrt(num); i <= limit; i++) {
-    if (num % i == 0)
-      return false;
-  }
-  return true;
-}
-
-console.log("Простые числа в диапазоне от 1 до 100: ");
-
-for (let num = 2; num < 101; num++) {
-  if (isPrime(num))
-    console.log(num + "  Делители этого числа: 1 и " + num);
-}
+  console.log(`%c${week[dayIndex]}`, styles);
+};
