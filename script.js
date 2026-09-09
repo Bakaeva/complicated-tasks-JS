0e2621ab5ba9876e7566774a62dad2e704b1f11b
Bakaeva <l_bakaeva74@mail.ru>
2026-09-09 22:28:05 +0300

    Выполнено усложненное д/з урока 9 (DOM)

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
 'use strict';
 
-// Создать массив week и записать в него дни недели в виде строк
-// Вывести на экран все дни недели
-// Каждый из них с новой строчки
-// Выходные дни - курсивом
-// Текущий день - жирным шрифтом(использовать объект даты)
+// 1) Выведите на страницу текущую дату и время в 2-х форматах:
+//   a) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды'
+//   б) '04.02.2020 - 21:05:33'
+// 2) Для вывода в формате (а) напишите функцию, которая будет менять склонение слов в зависимости от числа, "час, часов, часа"
+// 3) Для вывода в формате (б) напишите функцию, которая будет добавлять 0 перед значениями которые состоят из одной цифры (из 9:5:3 1.6.2019 сделает 09:05:03 01.06.2019)
+// 4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду
 
-const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-let todayIndex = new Date().getDay();
-todayIndex = todayIndex === 0 ? 6 : todayIndex - 1;
+/**
+ * Функция выбора одной из 3-х возможных строк (переданных в параметрах one, two, five), следующей после num и зависящей от значения num (выбор последующей строки в нужном падеже)
+ * @param {number} num - числовое значение, от которого зависит, в каком падеже будет последующая строка
+ * @param {*} one - строка, следующая за числами, оканчивающимися на 1 (кроме 11) 
+ * @param {*} two - строка, следующая за числами, оканчивающимися на 2,3,4 (кроме 12, 13, 14) 
+ * @param {*} five - строка, следующая за всеми остальными числами, включая 11, 12, 13, 14
+ * @returns {string} - одна из 3-х строк, переданных в параметрах one, two, five
+ */
+function getCorrespondingValue(num, one, two, five) {
+  num %= 100;
 
-for (const dayIndex in week) {
-  let styles = '';
-  if (dayIndex >= 5) {
-    styles += 'font-style: italic; ';
+  if (num >= 5 && num <= 20) {
+    return five;
   };
-  if (dayIndex == todayIndex) {
-    styles += 'font-weight: bold; ';
+
+  num %= 10;
+
+  if (num === 1) {
+    return one;
   };
 
-  console.log(`%c${week[dayIndex]}`, styles);
+  if (num >= 2 && num <= 4) {
+    return two;
+  };
+
+  return five;
 };
+
+/**
+ * Функция добавления 0 перед значениями, которые состоят из одной цифры
+ * @param {number} num - число из даты в диапазоне 1..31
+ * @returns {string} - число в виде строки из двух символов
+ */
+function addStartZero(num) {
+  return num < 10 ? '0' + num : num;
+};
+
+function updateDateTime() {
+  console.clear();
+
+  const now = new Date();
+  const dayOfWeek = daysOfWeek[now.getDay()] // .getDay() возвращает порядковый номер дня недели (нумерация с нуля, 0 ~ воскресенье)
+  const day = now.getDate();
+  const month = now.getMonth(); // порядковый номер месяца (нумерация с нуля, 0 ~ январь)
+  const year = now.getFullYear();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  console.log(`Сегодня ${dayOfWeek}, ${day} ${months[month]} ${year} года, ${hours} ${getCorrespondingValue(hours, "час", "часа", "часов")} ${minutes} ${getCorrespondingValue(minutes, "минута", "минуты", "минут")} ${seconds} ${getCorrespondingValue(seconds, "секунда", "секунды", "секунд")}`);
+  console.log(`${addStartZero(day)}.${addStartZero(month + 1)}.${year} - ${addStartZero(hours)}:${addStartZero(minutes)}:${addStartZero(seconds)}`);
+};
+
+setInterval(updateDateTime, 1000);
